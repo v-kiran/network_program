99e5a7b5c9af2239cd3ec08a0d35c08dd518d077
kiran <kiranvannalath@gmail.com>
2020-05-22 12:31:20 +0530

    automation test

---
+++ problem1-part2.php
@@ -0,0 +1,23 @@
+<?php
+// Your code here!
+
+$txt = file_get_contents($argv[1]);
+
+
+$matches = $matches1 =$matches2 = $matches3 = array();
+$output = array();
+$output['Loopback0'] = preg_match('/interface Loopback(.*)\n ip.address.(.*)/', $txt, $matches);
+$output['Loopback1'] = preg_match('/interface Loopback(.*)\n ip.address.(.*)/', $txt, $matches1);
+$output['GigabitEthernet1'] = preg_match('/interface GigabitEthernet1(.*)\n ip.address.(.*)/', $txt, $matches2);
+$output['GigabitEthernet2'] = preg_match('/interface GigabitEthernet2(.*)\n ip.address.(.*)/', $txt, $matches3);
+
+$output['Loopback0'] = $matches[2];
+$output['Loopback1'] = $matches1[2];
+$output['GigabitEthernet1'] = substr($matches2[2], 0, strrpos($matches2[2], ' '));
+$output['GigabitEthernet2'] = substr($matches3[2], 0, strrpos($matches3[2], ' '));
+
+$myJSON = json_encode($output);
+
+var_dump($output);
+
+?>
